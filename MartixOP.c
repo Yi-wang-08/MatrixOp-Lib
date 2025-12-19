f28e1221ffa3b42e31d69985df1a23a704f2de18
Wang <wangstudy48@gmail.com>
2025-12-19 19:05:16 +0800

    mulelm

--- MartixOP.c
+++ MartixOP.c
@@ -22,7 +22,7 @@ for(int j=0;j<SIZE;j++){
 C[i][j] = A[i][j] * B[i][j]; 
  } 
  } 
-}
+}//mulelm
 void mul(int A[SIZE][SIZE],int B[SIZE][SIZE],int C[SIZE][SIZE]){ 
  for (int i = 0;i<SIZE;i++){ 
 for(int j=0;j<SIZE;j++){ 
